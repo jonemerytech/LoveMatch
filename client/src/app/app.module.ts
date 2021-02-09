834526f136980e70003dcd2853959ed8d66e84ae
Jonathan <jehoopz92@gmail.com>
2021-02-09 09:34:35 -0500

    Updating Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from "./nav/nav.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [AppComponent, NavComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
