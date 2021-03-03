bc67fa71d4268bdae25200f565d39ca97d5ccf0b
Jonathan <jehoopz92@gmail.com>
2021-03-03 09:17:59 -0500

    End of Section 10

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -48,7 +48,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid username");
 
--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -33,13 +33,13 @@ namespace API.Data {
             return await _context.Users.FindAsync (id);
         }
 
-        // public async Task<AppUser> GetUserByUsernameAsync (string username) {
-        //     return await _context.Users.Include (p => p.Photos).SingleOrDefaultAsync (x => x.UserName == username);
-        // }
+        public async Task<AppUser> GetUserByUsernameAsync (string username) {
+            return await _context.Users.Include (p => p.Photos).SingleOrDefaultAsync (x => x.UserName == username);
+        }
 
-        // public async Task<IEnumerable<AppUser>> GetUsersAsync () {
-        //     return await _context.Users.Include (p => p.Photos).ToListAsync ();
-        // }
+        public async Task<IEnumerable<AppUser>> GetUsersAsync () {
+            return await _context.Users.Include (p => p.Photos).ToListAsync ();
+        }
 
         public async Task<bool> SaveAllAsync () {
             return await _context.SaveChangesAsync () > 0;
